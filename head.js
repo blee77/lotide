c529f778362f312657de37a160ff682c10773416
Bruce Lee <brucehlee@yahoo.ca>
2023-01-27 12:23:14 -0500

    Module 1 - Implement head added

---
+++ head.js
@@ -0,0 +1,22 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed :  ${actual}   ===  ${expected}`);
+  } else if (actual !== expected) {
+    console.log(` 🛑🛑🛑 Assertion Failed : ${actual}  !==  ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (array[0] === null) {
+    return "undefined";
+  } else {
+    return array[0];
+  }
+  
+};
+
+assertEqual(head([]),5);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
