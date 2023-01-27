9dc36dfb3de6b0f6c9d9cd6fec3e7090d7340689
Bruce Lee <brucehlee@yahoo.ca>
2023-01-27 13:54:49 -0500

    Module 1 - Added tail.js

---
+++ tail.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  console.log("actual", actual);
+  console.log("expected", expected);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed :  ${actual}   ===  ${expected}`);
+  } else  {
+    console.log(` 🛑🛑🛑 Assertion Failed : ${actual}  !==  ${expected}`);
+  }
+};
+const tail = function(array) {
+  if (array.length < 1) {
+    return [];
+  }
+  // console.log(array.slice(1));
+  return array.slice(1);
+};
+let words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail([1,2,3,4,5,6]));
+words = tail(words);
+assertEqual(words.length, 3);
+
+let numbers = [1,2,3,4,5];
+// console.log(tail([1,2,3,4,5,6]));
+numbers = tail(numbers);
+assertEqual(numbers.length, 4);
