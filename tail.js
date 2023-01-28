d00e282120df161ad06689c42ced0b720dd39402
Bruce Lee <brucehlee@yahoo.ca>
2023-01-28 12:59:05 -0500

    Module 1 Updated tail.js

--- tail.js
+++ tail.js
@@ -15,11 +15,9 @@ const tail = function(array) {
   return array.slice(1);
 };
 let words = ["Yo Yo", "Lighthouse", "Labs"];
-
 words = tail(words);
 assertEqual(words.length, 3);
 
 let numbers = [1,2,3,4,5];
-
 numbers = tail(numbers);
 assertEqual(numbers.length, 4);
