dc699740827aaf9aeed7a5eac177f32eaf32546f
Bruce Lee <brucehlee@yahoo.ca>
2023-02-04 12:33:31 -0500

    Module 1 Week2 - Added assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,43 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed :  ${actual}   ===  ${expected}`);
+//   } else  {
+//     console.log(` 🛑🛑🛑 Assertion Failed : ${actual}  !==  ${expected}`);
+//   }
+// };
+
+
+const eqObjects = function(object1, object2) {
+  console.log(object1);
+  console.log(object2);
+
+  let array1 = Object.keys(object1);
+  let array2 = Object.keys(object2);
+
+  for (let i of array1) {
+    if (array1.length === array2.length) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  // Implement me!
+
+  let areObjectsEqual = eqObjects(actual, expected);
+  //Implement eqObjects
+
+  const inspect = require('util').inspect; // <= add this line
+  if (areObjectsEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} ===  ${inspect(expected)}`);
+  } else if (actual !== expected) {
+    console.log(` 🛑🛑🛑 Assertion Failed : ${inspect(actual)} ===  ${inspect(expected)}`);
+  }
+};
+
+
+
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }); // => should PASS
