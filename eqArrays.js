96f9a8019fe070274a85d48d4fac910d997cb297
Bruce Lee <brucehlee@yahoo.ca>
2023-02-15 00:30:14 -0500

    W4 M1 - Refactor eqArrays added

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed :  ${actual}   ===  ${expected}`);
-  } else  {
-    console.log(` 🛑🛑🛑 Assertion Failed : ${actual}  !==  ${expected}`);
-  }
-};
+
 
 
 
@@ -18,10 +12,4 @@ const eqArrays = function(array1, array2) {
   
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASSs
+module.exports = eqArrays;
